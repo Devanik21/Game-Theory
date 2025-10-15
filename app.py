9e8337fc78788bbe8d000fc562cb6bb48d47307f
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-10-15 11:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1232,7 +1232,7 @@ elif category == "Voting Systems":
                 for cand, votes in vote_counts.items():
                     if votes > n_voters / 2:
                         st.success(f"🏆 Winner: Candidate {chr(65 + cand)} with {votes} votes!")
-                        st.balloons()
+                        
                         return
                 
                 # Elimination
